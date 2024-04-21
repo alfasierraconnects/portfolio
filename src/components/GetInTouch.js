fbecc559bc2d96f713f77c7162b68daf6138f9dd
alfasierraconnects <alfasierraconnects@gmail.com>
2024-04-21 20:36:58 +0530

    Added funtionality of sending message

--- src/components/GetInTouch.js
+++ src/components/GetInTouch.js
@@ -1,12 +1,38 @@
 import React from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const GetInTouch = () => {
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "b74727bc-561d-47f5-bfd0-25c733679e36");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      event.target.reset();
+      toast.success("Message sent successfully. Thank You!");
+    } else {
+      toast.error("Error sending Message. Pardon me!");
+    }
+  };
   return (
     <div className="text-gray-300 mt-16 my-10">
+      <ToastContainer />
       <h4 className="text-xl underline underline-offset-4 decoration-blue-400 font-semibold text-center">
         Get in touch with me
       </h4>
-      <form className="flex flex-col container w-[90%] sm:w-[80%] lg:w-[66%] mx-auto">
+      <form
+        onSubmit={onSubmit}
+        className="flex flex-col container w-[90%] sm:w-[80%] lg:w-[66%] mx-auto"
+      >
         <label className="mt-6 font-medium" htmlFor="name">
           Your name:
         </label>
